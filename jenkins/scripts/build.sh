0eb6a46a1f07ab43aab51f3e0d6b0be2ff4685f8
thanawis <thana.wisutphusi@gmail.com>
2022-09-08 01:34:22 +0700

    remove no-cache

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker build -t chowkong_client --no-cache -f ${JENKINS_HOME}/workspace/${JOB_NAME}/jenkins/client/Dockerfile ${JENKINS_HOME}/workspace/${JOB_NAME}/client
+docker build -t chowkong_client -f ${JENKINS_HOME}/workspace/${JOB_NAME}/jenkins/client/Dockerfile ${JENKINS_HOME}/workspace/${JOB_NAME}/client
 
-docker build -t chowkong_backend --no-cache -f ${JENKINS_HOME}/workspace/${JOB_NAME}/jenkins/backend/Dockerfile ${JENKINS_HOME}/workspace/${JOB_NAME}/backend
+docker build -t chowkong_backend -f ${JENKINS_HOME}/workspace/${JOB_NAME}/jenkins/backend/Dockerfile ${JENKINS_HOME}/workspace/${JOB_NAME}/backend
 
-docker build -t chowkong_nginx --no-cache -f ${JENKINS_HOME}/workspace/${JOB_NAME}/jenkins/nginx/Dockerfile ${JENKINS_HOME}/workspace/${JOB_NAME}/nginx
+docker build -t chowkong_nginx -f ${JENKINS_HOME}/workspace/${JOB_NAME}/jenkins/nginx/Dockerfile ${JENKINS_HOME}/workspace/${JOB_NAME}/nginx
--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker-compose -f docker-compose-prod.yml down --remove-orphans --rmi all
+docker-compose -f docker-compose-prod.yml down --remove-orphans
 
 docker-compose -f docker-compose-prod.yml up -d --remove-orphans
