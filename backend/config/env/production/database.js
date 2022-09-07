d78dfd3dcee4c2ee98d3a81d2c001c277d893716
thanawis <thana.wisutphusi@gmail.com>
2022-09-07 17:43:32 +0700

    add backend conf env

---
+++ backend/config/env/production/database.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+    },
+    ssl: {
+      ca: fs.readFileSync(`${__dirname}/ca-certificate.crt`).toString(),
+    },
+    debug: false,
+  },
+});
