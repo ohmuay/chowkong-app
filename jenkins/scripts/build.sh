0faf9310ff4c40a8760887759b73270a06af33b9
thanawis <thana.wisutphusi@gmail.com>
2022-09-08 00:29:51 +0700

    add ENV

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker build --build-arg JOB_NAME=${JOB_NAME} --build-arg JENKINS_HOME=${JENKINS_HOME}-t chowkong_client --no-cache -f ${JENKINS_HOME}/workspace/${JOB_NAME}/jenkins/client/Dockerfile .
+docker build -t chowkong_client --no-cache --build-arg JOB_NAME=${JOB_NAME} --build-arg JENKINS_HOME=${JENKINS_HOME} -f ${JENKINS_HOME}/workspace/${JOB_NAME}/jenkins/client/Dockerfile .
 
-docker build --build-arg JOB_NAME=${JOB_NAME} --build-arg JENKINS_HOME=${JENKINS_HOME}-t chowkong_backend --no-cache -f ${JENKINS_HOME}/workspace/${JOB_NAME}/jenkins/backend/Dockerfile .
+docker build -t chowkong_backend --no-cache --build-arg JOB_NAME=${JOB_NAME} --build-arg JENKINS_HOME=${JENKINS_HOME} -f ${JENKINS_HOME}/workspace/${JOB_NAME}/jenkins/backend/Dockerfile .
 
-docker build --build-arg JOB_NAME=${JOB_NAME} --build-arg JENKINS_HOME=${JENKINS_HOME}-t chowkong_nginx --no-cache -f ${JENKINS_HOME}/workspace/${JOB_NAME}/jenkins/nginx/Dockerfile .
+docker build -t chowkong_nginx --no-cache --build-arg JOB_NAME=${JOB_NAME} --build-arg JENKINS_HOME=${JENKINS_HOME} -f ${JENKINS_HOME}/workspace/${JOB_NAME}/jenkins/nginx/Dockerfile .
