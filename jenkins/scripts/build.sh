1826f0b14912e8df9a6206416bbef5145d9a12a6
thanawis <thana.wisutphusi@gmail.com>
2022-09-08 00:07:30 +0700

    remove double quotes

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker build -t chowkong_client --build-arg JOB_NAME="${JOB_NAME}" --build-arg JENKINS_HOME="${JENKINS_HOME}" -f ${JENKINS_HOME}/workspace/${JOB_NAME}/jenkins/client/Dockerfile .
+docker build -t chowkong_client --build-arg JOB_NAME=${JOB_NAME} --build-arg JENKINS_HOME=${JENKINS_HOME} -f ${JENKINS_HOME}/workspace/${JOB_NAME}/jenkins/client/Dockerfile .
 
-docker build -t chowkong_backend --build-arg JOB_NAME="${JOB_NAME}" --build-arg JENKINS_HOME="${JENKINS_HOME}" -f ${JENKINS_HOME}/workspace/${JOB_NAME}/jenkins/backend/Dockerfile .
+docker build -t chowkong_backend --build-arg JOB_NAME=${JOB_NAME} --build-arg JENKINS_HOME=${JENKINS_HOME} -f ${JENKINS_HOME}/workspace/${JOB_NAME}/jenkins/backend/Dockerfile .
 
-docker build -t chowkong_nginx --build-arg JOB_NAME="${JOB_NAME}" --build-arg JENKINS_HOME="${JENKINS_HOME}" -f ${JENKINS_HOME}/workspace/${JOB_NAME}/jenkins/nginx/Dockerfile .
+docker build -t chowkong_nginx --build-arg JOB_NAME=${JOB_NAME} --build-arg JENKINS_HOME=${JENKINS_HOME} -f ${JENKINS_HOME}/workspace/${JOB_NAME}/jenkins/nginx/Dockerfile .
