c7c36d1afb6035074f2053d76f6be47f3b0961a1
thana.wisutphusi <thana.wisutphusi@thoughtworks.com>
2022-09-08 13:17:16 +0700

    add test to pipeline

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo "testing"
+docker build -t chowkong_client-test -f ${JENKINS_HOME}/workspace/${JOB_NAME}/jenkins/client/Dockerfile.test ${JENKINS_HOME}/workspace/${JOB_NAME}/client
+
+docker run chowkong_client-test -d -rm
