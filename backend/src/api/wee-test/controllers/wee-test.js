7bef01c59986f1b9ece8db374b4bef1ede9e25c2
Weelawawt Sampao <Weelewat@gmail.com>
2022-10-17 21:04:51 +0700

    wee test first commit

---
+++ backend/src/api/wee-test/controllers/wee-test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  wee-test controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::wee-test.wee-test');
